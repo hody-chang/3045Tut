9f2a451b22de1fef65838309f18dea66aef85e29
hody-chang <19203993@life.hkbu.edu.hk>
2022-10-10 13:59:54 +0800

    Update

--- Tut2.py
+++ Tut2.py
@@ -11,20 +11,14 @@ theta[0] = np.pi/2
 v = np.zeros(n_inter)
 a = np.zeros(n_inter)
 
-
-
 for i in range(n_inter-1):
     a[i] = -9.81*np.sin(theta[i])
     v[i+1] = v[i] + a[i] * dt
     theta[i+1] = theta[i] + v[i] * dt
-    #t[i+1] = t[i] + dt
 
-#fig, ax = plt.subplots()
-
-
-plt.plot(t,theta, label = "Angle")
-plt.plot(t,v,label = "Angular velcity")
-plt.plot(t,a,label = "Acc")
+plt.plot(t, theta, label = "Angle")
+plt.plot(t, v, label = "Angular velocity")
+plt.plot(t, a, label = "Acc")
 plt.xlabel("Time")
 plt.legend()
 plt.show()
