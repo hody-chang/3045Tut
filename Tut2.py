5c2c3f81a949557a4f2f4031e5966f455214a19c
hody-chang <107836041+hody-chang@users.noreply.github.com>
2022-10-17 14:41:44 +0800

    Update Tut2.py

--- Tut2.py
+++ Tut2.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
-n_inter = 10000
+n_inter = 60000
 dt = 0.0005
 theta = np.zeros(n_inter)
 t = np.arange(0, n_inter * dt, dt)
+L = 1
+
 
 theta[0] = np.pi / 2
 
@@ -16,19 +18,26 @@ a = np.zeros(n_inter)
 d = np.zeros((n_inter, 2))
 
 for i in range(n_inter - 1):
-    a[i] = -9.81 * np.sin(theta[i])
+    a[i] = -9.81 / L * np.sin(theta[i])
     v[i + 1] = v[i] + a[i] * dt
     theta[i + 1] = theta[i] + v[i] * dt
     d[i] = [np.cos(theta[i]-np.pi/2), np.sin(theta[i]-np.pi/2)]
-'''
-plt.plot(t, theta, label = "Angle")
-plt.plot(t, v, label = "Angular velocity")
-plt.plot(t, a, label = "Acc")
-plt.xlabel("Time")
-plt.legend()
-plt.show()
-'''
 
+
+fig, (ax1, ax2, ax3) = plt.subplots(3)
+ax1.plot(t, theta)
+ax1.set_title("Angle (Radian)")
+ax2.plot(t, v)
+ax2.set_title("Angular velocity (Radian/s)")
+ax3.plot(t, a)
+ax3.set_title("Acceleration (Radian/s^2)")
+ax1.set(xlabel='time')
+ax2.set(xlabel='time')
+ax3.set(xlabel='time')
+plt.tight_layout()
+plt.show()
+
+'''
 # Animation
 
 fig = plt.figure(figsize=(5, 4))
@@ -50,3 +59,4 @@ def animate(i):
 animation = FuncAnimation(
     fig, animate, len(d), interval=dt*100, blit=True)
 plt.show()
+'''
