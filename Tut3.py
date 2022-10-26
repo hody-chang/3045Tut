72ea3ce4c28ec58a60d7aa6cb718a710686c344e
hody-chang <19203993@life.hkbu.edu.hk>
2022-10-27 02:38:35 +0800

    Update

--- Tut3.py
+++ Tut3.py
@@ -6,11 +6,12 @@ from numpy import ndarray
 
 a0 = 1
 a1 = -a0
-E = -1/4
-a2 = 1/6*a0*(2-E)
-a3 = -1/18*a0*(1-2*E)
-r0 = 0.1
-r = np.arange(start=0, stop=r0, step=0.0001)
+E = 1.2
+a2 = a0*(2-E)/6
+a3 = -a0*(1-2*E)/18
+r0 = 0.01
+dr = 0.0001
+r = np.arange(start=0, stop=r0, step=dr)
 R = np.empty_like(r)
 e = np.empty_like(r)
 v = np.empty_like(r)
@@ -24,30 +25,28 @@ plt.plot(r, R, label="Series Solution")
 plt.plot(r, e, label="exact Solution")
 plt.legend()
 plt.show()
-
 '''
+
 # 1b) shooting method
 
-dr = 0.0001
+
 L = 10
-E0 = E
-r2 = np.arange(start=0.01, stop=L, step=dr)
-R2= np.zeros(np.shape(r2), dtype="float")
+r2 = np.arange(start=0.01, stop=L+dr, step=dr)
+R2 = np.zeros(np.shape(r2), dtype="float")
+e2 = np.zeros(np.shape(r2), dtype="float")
 v2 = v[-1]
 R2[0] = R[-1]
-force = 0.0
+force = -2*v2/r2[0]-(E+2/r2[0])*R2[0]
 
-for i in range(r2.size-1):
-    R_pre = R2[i] + v2*dr
+for i in range(1, r2.size):
+    R_pre = R2[i-1] + v2*dr
     v_pre = v2+force*dr
-    R2[i+1] = R2[i] + 0.5*(v2+v_pre)*dr
-    force_new = -2*v_pre/r2[i+1]-(E0+2/r2[i+1])*R_pre
+    R2[i] = R2[i-1] + 0.5*(v2+v_pre)*dr
+    force_new = -2*v_pre/r2[i]-(E+2/r2[i])*R_pre
     v = v+0.5*(force+force_new)*dr
     force = force_new
 
-
-print(R2)
-
+print(force)
 plt.plot(r2, R2, label="Shooting(E0=-1)")
 plt.legend()
 plt.show()
