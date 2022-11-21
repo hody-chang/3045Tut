90f8380d534080005abbca971ea2de2c640d2c18
hody-chang <19203993@life.hkbu.edu.hk>
2022-11-21 21:59:51 +0800

    Update

---
+++ Tut4_random.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+import random
+
+
+
+length, width = 100, 100
+
+grids = np.zeros((length, width))
+current_x, current_y = 0, 0
+grids[current_x, current_y] = 1
+def movement(direction):
+    global grids, current_x, current_y, length, width
+    #grids[current_x, current_y] = 0
+    if direction == 0:
+        current_x, current_y = current_x - 1, current_y
+    elif direction == 1:
+        current_x, current_y = current_x + 1, current_y# input your code
+    elif direction == 2:
+        current_x, current_y = current_x, current_y - 1# input your code
+    elif direction == 3:
+        current_x, current_y = current_x, current_y + 1# input your code
+    else:
+        pass
+        # stay
+    if current_x >= length:
+        current_x = length - 1 - (current_x - (length - 1))
+    elif current_x < 0:
+        current_x = - current_x
+    else:
+        pass
+    if current_y >= width:
+        current_y = width - 1 - (current_y - (width - 1))
+    elif current_y < 0:
+        current_y = - current_y
+    else:
+        pass
+    grids[current_x, current_y] += 1
+
+
+#initialize the position of the object
+plt.figure()
+fig = plt.gcf()
+ax = plt.gca()
+ax_update = ax.imshow(grids, vmin=0, vmax=15)
+def run(i):
+    global grids, x, y, ax_update
+    movement(random.randint(0, 4))
+    ax_update.set_data(grids)
+    return ax_update
+
+ani = animation.FuncAnimation(fig, run, interval = 10)
+plt.show()
